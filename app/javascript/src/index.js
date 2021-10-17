1f90deeec5f6d8fea0a3d14702eaee7997e4d8f9
Andre Cesar Gomes <andre270492@gmail.com>
2021-10-17 14:51:27 -0300

    Add React setup with hot reload

---
+++ app/javascript/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+import React from 'react';
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <h1>Olá Mundo!</h1>,
+    document.querySelector("#react-root")
+  );
+});
